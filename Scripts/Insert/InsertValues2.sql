07511f489f9ce52fd94ab9788481fbdd79035f0b
Gabriel Lima <lima.gabrieldsantos@gmail.com>
2023-03-27 10:59:01 -0400

    add insertions

--- Scripts/Insert/InsertValues2.sql
+++ Scripts/Insert/InsertValues2.sql
@@ -1,96 +1,44 @@
--- Active: 1679925232147@@127.0.0.1@3306
+-- Active: 1679926847247@@127.0.0.1@3306
 INSERT INTO "Operadoras"(CNPJ, email, registro_de_operadora, nome_fantasia, telefone) 
 VALUES ("33333333333333", 'exemplo@gmail.com', 001922, 'Hapvida Assistência Médica', '92911111111'),
     ("00000000000000", 'exemplo@gmail.com', 001924, 'Bradesco Saúde', '92911111112'),
     ("11111111111111", 'exemplo@gmail.com', 001923, 'Amil Assistência Médica', '92911111113');
 
-INSERT INTO Plano
+INSERT INTO PlanoDeSaude (preco, tipo, titulo, CNPJ) 
+VALUES (120.5, 'I', 'Plano de Exemplo', '33333333333333'),
+    (220.5, 'E', 'Plano de Exemplo', '33333333333333');
 
-CREATE TABLE PlanoDeSaude (
-  id INTEGER PRIMARY KEY AUTOINCREMENT, 
-  titulo VARCHAR(100) NOT NULL, 
-  preco DOUBLE NOT NULL, 
-  tipo VARCHAR(2) NOT NULL, 
-  CNPJ CHAR(14) NOT NULL,
-  FOREIGN KEY(CNPJ) REFERENCES Operadoras(CNPJ)
-);
 
-CREATE TABLE Farmacias (
-  CNPJ CHAR(14) PRIMARY KEY, 
-  nome_empresarial VARCHAR(100) NOT NULL, 
-  nome_fantasia VARCHAR(100) NOT NULL, 
-  CNES INT NOT NULL, 
-  municipio VARCHAR(100) NOT NULL, 
-  numero INT NOT NULL, 
-  CEP CHAR(8) NOT NULL, 
-  UF CHAR(2) NOT NULL, 
-  logradouro VARCHAR(100) NOT NULL
-);
+INSERT INTO "Farmacias"(CEP, CNES, CNPJ, logradouro, municipio, nome_empresarial, nome_fantasia, numero, UF)
+VALUES ('11111111', 001, '22222222222222', 'Rua Girassol', 'Manaus', 'FAA', 'Farmácia 1', 1, 'AM'),
+    ('11111112', 001, '22222222222223', 'Rua Girassol', 'Manaus', 'FBB', 'Farmácia 2', 2, 'SP');
 
-CREATE TABLE Medicamentos (
-  id_medicamento INTEGER PRIMARY KEY AUTOINCREMENT, 
-  nome VARCHAR(100) NOT NULL, 
-  preco DOUBLE NOT NULL, 
-  tipo CHAR NOT NULL, 
-  diretriz_de_utilizacao VARCHAR(100) NOT NULL
-);
+INSERT INTO "Medicamentos"(nome, preco, tipo, diretriz_de_utilizacao)
+VALUES ('MED1', 12.4, 'A', 'Controlado'),
+    ('MED2', 12.4, 'B', 'Controlado');
 
-CREATE TABLE EstabelecimentoMedico (
-  CNPJ CHAR(14) PRIMARY KEY, 
-  nome_empresarial VARCHAR(100) NOT NULL, 
-  nome_fantasia VARCHAR(100) NOT NULL
-  natureza_juridica CHAR(2) NOT NULL, 
-  CNES INT NOT NULL, 
-  tipo_de_gestao CHAR NOT NULL, 
-  logradouro VARCHAR(100) NOT NULL, 
-  UF CHAR(2) NOT NULL, 
-  CEP CHAR(8) NOT NULL, 
-  número INT NOT NULL
-);
+INSERT INTO EstabelecimentoMedico(CNPJ, nome_empresarial, natureza_juridica, nome_fantasia, CNES, tipo_de_gestao, logradouro,UF, CEP, número)
+VALUES ('11111111111114', 'MED1', 'SL', 'Medico Manaus', 002, 'P', 'Rua jasmin', 'AM', '11111111', 1),
+    ('11111111111115', 'MED2', 'EE', 'Medico Amazonas', 003, 'P', 'Rua Anacora', 'AM', '11111112', 4);
 
-CREATE TABLE Procedimentos (
-  id_procedimento INTEGER PRIMARY KEY AUTOINCREMENT, 
-  descricao TEXT NOT NULL, 
-  preco DOUBLE NOT NULL, 
-  subtipo_do_procedimento VARCHAR(100) NOT NULL, 
-  tipo VARCHAR(50) NOT NULL, 
-  segmento VARCHAR(100) NOT NULL, 
-  PAC BOOLEAN NOT NULL, 
-  diretriz_de_utilizacao VARCHAR(100) NOT NULL,
-);
+INSERT INTO "Procedimentos"(descricao, diretriz_de_utilizacao, "PAC", segmento, preco, subtipo_do_procedimento,tipo)
+VALUES ('Cirurgia de braço', 'Controlado', 1, 'ambulatoria', 1000, 'cirurgia', 'procedimento cirúrgico'),
+    ('Cirurgia de perna', 'Controlado', 1, 'ambulatoria', 1000, 'cirurgia', 'procedimento cirúrgico');
 
-CREATE TABLE PlanoHospitalar (
-  CNPJ CHAR(14), 
-  id INT,
-  PRIMARY KEY(CNPJ, id),
-  FOREIGN KEY(CNPJ) REFERENCES EstabelecimentoMedico(CNPJ),
-  FOREIGN KEY(id) REFERENCES PlanoDeSaude(id),
-);
+SELECT * FROM "EstabelecimentoMedico";
+SELECT * FROM "PlanoDeSaude";
 
-CREATE TABLE PlanoFarmaceutico (
-  CNPJ CHAR(14), 
-  id INT,
-  PRIMARY KEY(CNPJ, id),
-  FOREIGN KEY(CNPJ) REFERENCES Farmacias(CNPJ),
-  FOREIGN KEY(id) REFERENCES PlanoDeSaude(id)
-);
+INSERT INTO "PlanoHospitalar"("CNPJ", id_plano)
+VALUES ('11111111111114', 1);
 
-CREATE TABLE ProcedimentosSuportados (
-  id_procedimento INT, 
-  CNPJ CHAR(14), 
-  id INT,
-  PRIMARY KEY(CNPJ, id, id_procedimento),
-  FOREIGN KEY(CNPJ) REFERENCES EstabelecimentoMedico(CNPJ),
-  FOREIGN KEY(id) REFERENCES PlanoDeSaude(id),
-  FOREIGN KEY(id_procedimento) REFERENCES Procedimentos(id_procedimento)
-);
+INSERT INTO "PlanoFarmaceutico"("CNPJ", id_plano)
+VALUES ('22222222222222', 1);
 
-CREATE TABLE MedicamentosSuportados (
-  id_medicamento INT, 
-  CNPJ CHAR(14), 
-  id INT,
-  PRIMARY KEY(CNPJ, id, id_medicamento),
-  FOREIGN KEY(CNPJ) REFERENCES EstabelecimentoMedico(CNPJ),
-  FOREIGN KEY(id) REFERENCES PlanoDeSaude(id),
-  FOREIGN KEY(id_medicamento) REFERENCES Medicamentos(id_medicamento)
-);
+
+SELECT * FROM "Procedimentos";
+
+INSERT INTO "ProcedimentosSuportados"("CNPJ", id_plano, id_procedimento)
+VALUES ('22222222222222', 1, 1);
+
+INSERT INTO "MedicamentosSuportados"("CNPJ", id_plano, id_medicamento)
+VALUES ('22222222222222', 1, 1);
